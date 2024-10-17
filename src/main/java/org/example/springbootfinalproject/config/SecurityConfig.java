eba617513f7970f1a116b873d90d315b6669c16c
nikishkaa <nikita.goutor@gmail.com>
2024-10-17 20:22:38 +0300

    Add spring security.

---
+++ src/main/java/org/example/springbootfinalproject/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package org.example.springbootfinalproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth ->
+                auth.requestMatchers("/register").permitAll().requestMatchers("home")
+                        .permitAll().requestMatchers("users").permitAll().requestMatchers("home-page")
+                        .permitAll().requestMatchers("password-recovery").permitAll()
+                        .requestMatchers("/css/**").permitAll().requestMatchers("/js/**").permitAll().requestMatchers("/img/**").permitAll());
+
+        // LOGIN
+        http.formLogin(formLogin ->
+                formLogin
+                        .loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/home", true)
+                        .permitAll());
+        // LOGOUT
+        http.logout(logout ->
+                logout.invalidateHttpSession(true).clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout").permitAll());
+        return http.build();
+    }
+
+
+}
