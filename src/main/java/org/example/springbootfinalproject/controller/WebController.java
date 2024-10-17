2b109b3e8661f7bb94aaa394a180a65e738dae5e
nikishkaa <nikita.goutor@gmail.com>
2024-10-17 20:13:49 +0300

    Update All form, Update web controller, Add Spring SecurityConfig.java.

--- src/main/java/org/example/springbootfinalproject/controller/WebController.java
+++ src/main/java/org/example/springbootfinalproject/controller/WebController.java
@@ -7,11 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
     @GetMapping("/home-page")
     public String homePage() {
-        return "html/home-page";
+        return "home-page";
     }
 
     @GetMapping("/login")
     public String loginPage() {
-        return "html/form/login-form";
+        return "form/sign-in-form";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "form/sign-up-form";
+    }
+
+    @GetMapping("/password-recovery")
+    public String passwordResetPage() {
+        return "form/password-recovery-form";
     }
 }
