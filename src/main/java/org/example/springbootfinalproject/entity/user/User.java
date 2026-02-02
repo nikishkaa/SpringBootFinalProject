95bb62b035f762e859e7a3d038a0de9e375b7864
nikishkaa <nikita.goutor@gmail.com>
2026-02-02 11:05:04 +0300

    -

--- src/main/java/org/example/springbootfinalproject/entity/user/User.java
+++ src/main/java/org/example/springbootfinalproject/entity/user/User.java
@@ -73,8 +73,7 @@ public class User {
     @Column(name = "created_ts", nullable = false)
     @CreationTimestamp
     private Timestamp createdTs;
-
-
+    
     @Column(name = "updated_ts", nullable = true)
     private Timestamp updatedTs;
     public User(String username, String password, String email, String phoneNumber) {
